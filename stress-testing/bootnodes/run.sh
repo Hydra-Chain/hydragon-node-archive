a2ba67b2527e86c9377fa38943051b22410ca230
Rosen Santev <r.ssantev@gmail.com>
2023-01-09 14:42:04 +0200

    Change log-level for bootnode 1

--- stress-testing/bootnodes/run.sh
+++ stress-testing/bootnodes/run.sh
@@ -1,1 +1,1 @@
-polygon-edge server --data-dir ./data-dir --chain genesis.json --grpc-address :10000 --libp2p 0.0.0.0:10001 --jsonrpc :10002 --seal
+polygon-edge server --data-dir ./data-dir --chain genesis.json --grpc-address :10000 --libp2p 0.0.0.0:10001 --jsonrpc :10002 --seal --log-level=DEBUG
