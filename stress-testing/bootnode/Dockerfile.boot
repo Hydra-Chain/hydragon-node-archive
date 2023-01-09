eece62838667f40e167c7430bf305702960cfbc0
Rosen Santev <r.ssantev@gmail.com>
2023-01-09 13:36:33 +0200

    finish stress-test setup

--- stress-testing/bootnode/Dockerfile.boot
+++
@@ -1,7 +0,0 @@
-FROM 0xpolygon/polygon-edge:latest
-
-COPY ./bootnode/secrets data-dir
-COPY genesis.json .
-COPY run.sh .
-
-ENTRYPOINT "./run.sh"
--- stress-testing/run.sh
+++ stress-testing/run.sh
@@ -1,1 +1,24 @@
-polygon-edge server --data-dir ./data-dir --chain genesis.json --grpc-address :10000 --libp2p :10001 --jsonrpc :10002 --seal
+# Generate private keys for node
+polygon-edge secrets init --data-dir data-dir
+
+# Add node env variables to the staking contract repo
+key=`cat data-dir/consensus/validator.key`
+printf "PRIVATE_KEYS=%s\n" $key >> ./staking/staking-contracts/.env
+
+output=$(polygon-edge secrets output --data-dir data-dir)
+bls_key=$(echo $output| cut -d' ' -f 12)
+printf "BLS_PUBLIC_KEY=%s\n" $bls_key >> ./staking/staking-contracts/.env
+address=$(echo $output| cut -d' ' -f 7)
+body="{\"address\": \"${address}\"}"
+echo $body
+
+# wait  few seconds so the tx-executor would be started
+sleep 5
+# Fund node request
+curl -v -XPOST -H 'Content-type: application/json' -d "$body" 'http://tx-executor:5000/fund-node'
+
+# Setup staking for node
+cd ./staking/staking-contracts && npm run build && npm run stake && npm run register-blskey
+cd ./../../
+
+polygon-edge server --data-dir ./data-dir --chain genesis.json --grpc-address :10000 --libp2p 0.0.0.0:10001 --jsonrpc :10002
