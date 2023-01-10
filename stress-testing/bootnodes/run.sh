a52d9b9450818cff3f18f44686802274bc0d50ac
Rosen Santev <77731162+R-Santev@users.noreply.github.com>
2023-01-10 16:32:32 +0200

    One-click setup for cluster of nodes (#1)
    Create docker containers and docker-compose configuration that uses a tx-executor service and staking-contracts repository for easily auto-deploying a cluster of nodes in order to optimize the CI/CD process and reduce the overhead

---
+++ stress-testing/bootnodes/run.sh
@@ -0,0 +1,1 @@
+polygon-edge server --data-dir ./data-dir --chain genesis.json --grpc-address :10000 --libp2p 0.0.0.0:10001 --jsonrpc :10002 --seal --max-inbound-peers 200 --log-level=DEBUG 
---
+++ stress-testing/test.sh
@@ -0,0 +1,1 @@
+polygon-edge server --data-dir ./data-dir --chain genesis.json --grpc-address :$GRPC_PORT --libp2p :$LIBP2P_PORT --jsonrpc :$JSONRPC_PORT --seal
