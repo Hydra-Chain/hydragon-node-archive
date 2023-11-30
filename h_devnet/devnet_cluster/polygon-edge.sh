a93f9630ac3479a9f01bb9f10025e0fed226df04
Rosen Santev <r.ssantev@gmail.com>
2023-11-30 13:46:06 +0200

    Modify devnet cluster setup
    Switch to root user to be able to install desired packages
    Expose libp2p port so nodes can be discovered outside of the bridged network
    The setup script now reuses already created secrets

--- h_devnet/devnet_cluster/polygon-edge.sh
+++ h_devnet/devnet_cluster/polygon-edge.sh
@@ -12,22 +12,31 @@ POLYGON_EDGE_BIN=polygon-edge
 CHAIN_CUSTOM_OPTIONS=$(
   tr "\n" " " <<EOL
 --block-gas-limit 10000000
---epoch-size 10
---chain-id 51001
+--epoch-size 100
+--chain-id 8844
 --name polygon-edge-docker
---premine 0x0000000000000000000000000000000000000000
---premine 0x211881Bb4893dd733825A2D97e48bFc38cc70a0c:0xD3C21BCECCEDA1000000
+--premine 0x211881Bb4893dd733825A2D97e48bFc38cc70a0c:0x314dc6448d932ae0a456589c0000
 --premine 0x8c293C5b70b6493856CF4C7419E1Fb137b97B25d:0xD3C21BCECCEDA1000000
---burn-contract 0:0x0000000000000000000000000000000000000000
 --proxy-contracts-admin 0x211881Bb4893dd733825A2D97e48bFc38cc70a0c
 EOL
 )
 
 case "$1" in
 "init")
-  echo "Generating PolyBFT secrets..."
-  secrets=$("$POLYGON_EDGE_BIN" polybft-secrets init --insecure --chain-id 51001 --num 5 --data-dir /data/data- --json)
-  echo "Secrets have been successfully generated"
+  # Check if secrets already exist
+  if [ -f /data/data-1/libp2p/libp2p.key ]; then
+    echo "PolyBFT secrets already exist, skipping secret generation."
+
+    # Loop through each data directory and delete specific subdirectories
+    for i in 1 2 3 4 5; do
+      echo "Cleaning up /data/data-$i directory..."
+      rm -rf /data/data-$i/blockchain /data/data-$i/consensus/polybft /data/data-$i/trie
+    done
+  else
+    echo "Generating PolyBFT secrets..."
+  fi
+
+  secrets=$("$POLYGON_EDGE_BIN" polybft-secrets init --insecure --chain-id 8844 --num 5 --data-dir /data/data- --json)
 
   rm -f /data/genesis.json
 
@@ -44,9 +53,6 @@ case "$1" in
     --bootnode "/dns4/node-3/tcp/1478/p2p/$(echo "$secrets" | jq -r '.[2] | .node_id')" \
     --bootnode "/dns4/node-4/tcp/1478/p2p/$(echo "$secrets" | jq -r '.[3] | .node_id')" \
     --bootnode "/dns4/node-5/tcp/1478/p2p/$(echo "$secrets" | jq -r '.[4] | .node_id')"
-
-  supernetID=$(cat /data/genesis.json | jq -r '.params.engine.polybft.supernetID')
-  addresses="$(echo "$secrets" | jq -r '.[0] | .address'),$(echo "$secrets" | jq -r '.[1] | .address'),$(echo "$secrets" | jq -r '.[2] | .address'),$(echo "$secrets" | jq -r '.[3] | .address')"
   ;;
 *)
   echo "Executing polygon-edge..."
